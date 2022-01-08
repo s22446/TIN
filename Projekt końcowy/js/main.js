562ff5f9df34b3225c02d02bda08156c4602e79e
Michal <s22446@pjwstk.edu.pl>
2022-01-08 02:34:22 +0100

    Visual improvements, added simple AI

--- Projekt końcowy/js/main.js
+++ Projekt końcowy/js/main.js
@@ -4,7 +4,17 @@ const game = {
     results: { x: 0, circle: 0 },
 };
 
-const settings = { startingSign: 'x' };
+const settings = {
+    startingSign: 'x',
+    opponent: 'human',
+    computerSign: undefined,
+};
+
+const board = {
+    'row-1': { 0: undefined, 1: undefined, 2: undefined },
+    'row-2': { 0: undefined, 1: undefined, 2: undefined },
+    'row-3': { 0: undefined, 1: undefined, 2: undefined },
+};
 
 const signLetter = { x: 'X', circle: 'O' };
 
@@ -13,11 +23,18 @@ const gameStatusText = {
     notStarted: 'The game has not started yet.',
 };
 
-const board = {
-    'row-1': { 0: undefined, 1: undefined, 2: undefined },
-    'row-2': { 0: undefined, 1: undefined, 2: undefined },
-    'row-3': { 0: undefined, 1: undefined, 2: undefined },
+const nextMoveText = {
+    player: 'Next move: player',
+    computer: 'Next move: computer',
 };
+
+function sleep(seconds) {
+    return new Promise((resolve) => setTimeout(resolve, seconds * 1000));
+}
+
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max + 1)) + min;
+}
 
 function clearBoardObject() {
     for (const row in board) {
@@ -42,6 +59,10 @@ function changeStartingSign() {
     $('#current-starting-sign span').text(signLetter[signToChange]);
 }
 
+function changeOpponentInfo() {
+    $('#current-opponent span').text(settings.opponent);
+}
+
 function updateResults() {
     $('#results-values').text(
         'X: ' + game.results.x + ' | O: ' + game.results.circle
@@ -54,12 +75,43 @@ function clearResults() {
     updateResults();
 }
 
+function switchOpponent() {
+    if (settings.opponent === 'human') {
+        settings.opponent = 'computer';
+        settings.computerSign = changeSign(settings.startingSign);
+
+        $('#computer-sign').click(changeComputerSign);
+        $('#computer-sign-info span').text(signLetter[settings.computerSign]);
+        $('#computer-sign, #computer-sign-info').removeClass('hidden');
+    } else {
+        settings.opponent = 'human';
+        $('#computer-sign').off('click');
+        $('#computer-sign-info span').empty();
+        $('#computer-sign, #computer-sign-info').addClass('hidden');
+    }
+
+    changeOpponentInfo();
+}
+
+function changeComputerSign() {
+    settings.computerSign = changeSign(settings.computerSign);
+    $('#computer-sign-info span').text(signLetter[settings.computerSign]);
+}
+
 async function showWinOrDrawNotification(mode) {
     var timeLeft = 2;
     var winningSign = signLetter[game.currentSign];
 
     if (mode === 'win') {
-        var text = winningSign + ' has won!';
+        if (settings.opponent === 'computer') {
+            if (game.currentSign === settings.computerSign) {
+                var text = 'Computer has won!';
+            } else {
+                var text = 'Player has won!';
+            }
+        } else {
+            var text = winningSign + ' has won!';
+        }
     } else if (mode === 'draw') {
         var text = 'Draw!';
     } else {
@@ -74,6 +126,7 @@ async function showWinOrDrawNotification(mode) {
         if (timeLeft <= 0) {
             $('.page-shadow, .page-shadow-text').removeClass('show');
             clearInterval(notificationTimer);
+            stopTheGame();
         }
         $('.page-shadow-text')
             .empty()
@@ -109,22 +162,51 @@ function compareSquares(squares) {
     return true;
 }
 
+async function computerMove() {
+    $('.game-squares, .game-square').addClass('computer-move');
+    $('#status-whose-move').text(nextMoveText.computer);
+
+    await sleep(1);
+    var freeSquares = $('.game-square').not('.sign-x, .sign-circle');
+    $(freeSquares[randomNumber(0, freeSquares.length - 1)]).click();
+
+    $('.game-squares, .game-square').removeClass('computer-move');
+    $('#status-whose-move').text(nextMoveText.player);
+}
+
 function startTheGame() {
     if (!game.hasStarted) {
         game.hasStarted = true;
         game.currentSign = settings.startingSign;
 
         $('#status-game-started').text(gameStatusText.started);
+        $('#status-opponent')
+            .addClass('filled')
+            .text('Opponent: ' + settings.opponent);
         $('#status-current-sign')
             .addClass('filled')
             .text('Current sign: ' + signLetter[game.currentSign]);
         $(
-            '#starting-sign-menubutton, #start-game-menubutton, #current-starting-sign, #clear-results-menubutton'
+            '#starting-sign-menubutton, #start-game-menubutton, #current-starting-sign, #clear-results-menubutton, #switch-opponent, #current-opponent, #computer-sign'
         )
             .addClass('hidden')
             .off('click');
         $('#stop-game-menubutton').show().click(stopTheGame);
         $('.game-square').removeClass('inactive');
+
+        if (
+            settings.opponent === 'computer' &&
+            settings.computerSign === game.currentSign
+        ) {
+            $('#status-whose-move')
+                .addClass('filled')
+                .text(nextMoveText.computer);
+            computerMove();
+        } else if (settings.opponent === 'computer') {
+            $('#status-whose-move')
+                .addClass('filled')
+                .text(nextMoveText.player);
+        }
     }
 }
 
@@ -136,11 +218,17 @@ function stopTheGame() {
         $('#status-game-started').text(gameStatusText.notStarted);
         $('#status-current-sign').removeClass('filled').empty();
         $(
-            '#starting-sign-menubutton, #start-game-menubutton, #current-starting-sign, #clear-results-menubutton'
+            '#starting-sign-menubutton, #start-game-menubutton, #current-starting-sign, #clear-results-menubutton, #switch-opponent, #current-opponent'
         ).removeClass('hidden');
         $('#start-game-menubutton').click(startTheGame);
         $('#starting-sign-menubutton').click(changeStartingSign);
         $('#clear-results-menubutton').click(clearResults);
+        if (settings.opponent === 'computer') {
+            $('#computer-sign').removeClass('hidden');
+            $('#computer-sign').click(changeComputerSign);
+            $('#status-whose-move').removeClass('filled').empty();
+        }
+        $('#switch-opponent').click(switchOpponent);
         $('#stop-game-menubutton').hide().off('click');
         $('.game-square')
             .addClass('inactive')
@@ -203,7 +291,6 @@ function gameWon() {
     game.results[game.currentSign]++;
     updateResults();
     showWinOrDrawNotification('win');
-    stopTheGame();
 }
 
 function squareClick() {
@@ -223,13 +310,19 @@ function squareClick() {
         } else {
             if (checkIfBoardIsFull()) {
                 showWinOrDrawNotification('draw');
-                stopTheGame();
             } else {
                 game.currentSign = changeSign(game.currentSign);
 
                 $('#status-current-sign').text(
                     'Current sign: ' + signLetter[game.currentSign]
                 );
+
+                if (
+                    settings.opponent === 'computer' &&
+                    game.currentSign === settings.computerSign
+                ) {
+                    computerMove();
+                }
             }
         }
     }
@@ -241,6 +334,7 @@ function prepareForStart() {
     $('#start-game-menubutton').click(startTheGame);
     $('#starting-sign-menubutton').click(changeStartingSign);
     $('#clear-results-menubutton').click(clearResults);
+    $('#switch-opponent').click(switchOpponent);
 }
 
 $(document).ready(function () {
