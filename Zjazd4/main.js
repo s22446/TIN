85bd3ae4766d6bb4daba8cc846dcb6251f339fe1
Michal <s22446@pjwstk.edu.pl>
2022-01-21 02:23:28 +0100

    Added week 4 exercises up to exercise 6

--- Zjazd4/main.js
+++ Zjazd4/main.js
@@ -57,3 +57,112 @@ function printDivisibleFromRange() {
             ).filter((value) => !(value % c))
     );
 }
+// Exercise 3
+function drawMultiplicationTable() {
+    var tableSize = getInputValue('3A');
+
+    var wallSign = ' | ';
+    var emptySpace = ' ';
+    var maxCellLength = Math.pow(tableSize, 2).toString().length;
+
+    var returnString = '';
+
+    for (var i = 1; i <= tableSize; i++) {
+        for (var j = 1; j <= tableSize; j++) {
+            var value = j * i;
+            var emptySpaceAmount = maxCellLength - value.toString().length;
+            returnString += value.toString();
+            if (emptySpaceAmount > 0) {
+                returnString += emptySpace.repeat(emptySpaceAmount);
+            }
+            returnString += wallSign;
+        }
+        returnString += '\n';
+    }
+
+    console.log(returnString);
+}
+// Exercise 4
+var fibonacciSequence = { 1: 1, 2: 1 };
+
+function fillFibonacciSequence(index) {
+    if (index < 3) {
+        return 1;
+    }
+
+    var result =
+        fillFibonacciSequence(index - 2) + fillFibonacciSequence(index - 1);
+
+    fibonacciSequence[index] = result;
+
+    return result;
+}
+
+function printFibonacciSequence() {
+    var sequenceLength = getInputValue('4A');
+
+    fillFibonacciSequence(sequenceLength);
+
+    if (sequenceLength < 2) {
+        console.log(sequenceLength + ' => ' + 1);
+    } else {
+        console.log(
+            sequenceLength +
+                ' => ' +
+                Object.keys(fibonacciSequence)
+                    .map(function (key) {
+                        return fibonacciSequence[key];
+                    })
+                    .join(', ')
+        );
+    }
+}
+
+// Exercise 5
+function printChristmasTree() {
+    var size = getInputValue('5A');
+    var starSign = '*';
+    var christmasTree = '';
+
+    for (i = 1; i <= size; i++) {
+        christmasTree += starSign.repeat(i) + '\n';
+    }
+
+    console.log(christmasTree);
+}
+
+// Exercise 6
+function printNightChristmasTree() {
+    var size = getInputValue('6A');
+    var treeSize = size - 2;
+    var treeBottomLength = treeSize * 2 - 1;
+    var drawingLength = treeBottomLength + 2;
+
+    var starSign = '*';
+    var treeSpace = ' ';
+    var christmasTree = '';
+
+    for (i = 1; i <= size; i++) {
+        if (i > 1) {
+            var backgroundWidth = treeSize - (i - 2);
+            var treeWidth = drawingLength - backgroundWidth * 2;
+        }
+
+        for (j = 1; j <= drawingLength; j++) {
+            if (i == 1 || i == size) {
+                christmasTree += starSign;
+            } else {
+                if (j <= backgroundWidth) {
+                    christmasTree += starSign;
+                } else if (j <= backgroundWidth + treeWidth) {
+                    christmasTree += treeSpace;
+                } else {
+                    christmasTree += starSign;
+                }
+            }
+        }
+        christmasTree += '\n';
+    }
+
+    console.log(size + ' => \n' + christmasTree);
+}
